2d96008a6f392750ac44a12cece6710c8391d637
maksim05073 <maksimmixcer@gmail.com>
2025-03-10 03:03:49 +0200

    Add files via upload

---
+++ Lb1_1/Lb1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace lb1_1
+{
+    internal class Program
+    {
+        static void Main()
+        {//f
+            Console.Write("Введіть значення a: ");
+            double a = float.Parse(Console.ReadLine());
+            Console.Write("Введіть значення b: ");
+            double b = float.Parse(Console.ReadLine());
+            double sum = a + b;
+            if (sum == 0)
+            {
+                Console.WriteLine("Помилка: ділення на нуль!");
+                return;
+            }
+            double result = Math.Pow(sum, 3) + a * Math.Cos(sum) + (a * Math.Sqrt(Math.Abs(b)) / sum);
+            Console.WriteLine($"Результат обчислення: {result}");
+        }
+    }
+}
---
+++ Lb1_2/Lb1_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Lb1_2
+{
+    public static void Main()
+    {
+        double y;
+        StreamReader input = new StreamReader("LAB2.txt");
+        StreamWriter output = new StreamWriter("RES.txt");
+        double x;
+        for (int i = 0; i < 9; i++)
+        {
+            x = double.Parse(input.ReadLine());
+            y = Math.Sqrt(x) * Math.Exp(-x);
+            output.WriteLine("***************************************");
+            output.WriteLine($"*    x={x}     y={y}    *");
+            output.WriteLine("***************************************");
+        }
+        output.WriteLine("Tаблицю склав студент ФЕКІ: Навроцький Максим");
+        output.Close();
+        input.Close();
+    }
+}
